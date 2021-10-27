277aa32b214fe351369d07f7b53b98e7e90f29ac
= <=>
2021-10-27 18:29:05 +0200

    progress on writing ast_file

--- src/pretty.ml
+++ src/pretty.ml
@@ -1,4 +1,4 @@
-
+open Ast
 open Lib
 open Format
 open Tast
@@ -26,12 +26,116 @@ let unop = function
 (* TODO afficher ast non type *)
 (* let ast_file fmt dl = ... *)
 
+let draw_constant : constant -> string = function
+   |Cbool(x) -> string_of_bool x
+   |Cint(x) -> Int64.to_string x
+   |Cstring(x) -> "\""^x^"\""
+
+
+let draw_unop fmt  = function
+   | Uneg -> pp_print_char fmt  '-'
+   | Unot -> pp_print_char fmt  '!'
+   | Uamp -> pp_print_char fmt  '&'
+   | Ustar-> pp_print_char fmt  '*'
+
+let draw_binop fmt = function
+   | Badd -> pp_print_string fmt "+"
+   | Bsub -> pp_print_string fmt "-"
+   | Bmul -> pp_print_string fmt "*"
+   | Bdiv -> pp_print_string fmt "/"
+   | Bmod -> pp_print_string fmt "%"
+   | Beq  -> pp_print_string fmt "="
+   | Bne  -> pp_print_string fmt "!="
+   | Blt  -> pp_print_string fmt "<" 
+   | Ble  -> pp_print_string fmt "<="
+   | Bgt  -> pp_print_string fmt ">"
+   | Bge  -> pp_print_string fmt ">="
+   | Band -> pp_print_string fmt "&&"
+   | Bor  -> pp_print_string fmt "||"
+
+let rec draw_pexpr (fmt : Format.formatter) e = 
+   match e.pexpr_desc with
+   | PEskip -> ()
+   | PEconstant(x) -> pp_print_string fmt (draw_constant x)
+   | PEbinop(op,e1,e2) -> 
+      draw_pexpr fmt e1;
+      draw_binop fmt op;
+      draw_pexpr fmt e2
+   | PEunop(op,e1) -> 
+      draw_unop fmt op;
+      draw_pexpr fmt e1
+   | PEnil -> pp_print_string fmt "NIL"
+(* | PEcall(_,pexprs) -> *) (*aucune idée de ce que c'est*)
+   | PEident(_) -> () 
+   | PEdot(e1,_) -> 
+      pp_print_char fmt '.';
+      draw_pexpr fmt e1;
+      pp_print_char fmt ' '
+
+   (*| PEassign of pexpr list * pexpr list
+   | PEvars of ident list * ptyp option * pexpr list*)
+
+   | PEif(cond,yes,no) -> 
+      pp_print_string fmt "if ("; 
+      draw_pexpr fmt cond;
+      pp_print_string fmt ")";
+      draw_pexpr fmt yes;
+      pp_print_string fmt "else";
+      draw_pexpr fmt no     
+
+   | PEreturn(pexprs) -> 
+      List.iter 
+         (fun e1 -> 
+            draw_pexpr fmt e1;
+            pp_print_char fmt ';') pexprs
+   | PEblock(pexprs) ->
+      pp_open_box fmt 5;
+      List.iter 
+         (fun e1 ->
+            draw_pexpr fmt e1)
+         pexprs;
+      pp_close_box fmt ()
+   | PEfor(loop,e1) -> 
+      pp_print_string fmt "for (";
+      draw_pexpr fmt loop;
+      pp_print_string fmt ") ";
+      draw_pexpr fmt e1
+   | PEincdec(e1,incdec) -> 
+      draw_pexpr fmt e1;
+      pp_print_string fmt
+         (if incdec = Inc then "++" else "--")
+   |_ -> ()
+
+let draw_pfunc (fmt : Format.formatter) (f : Ast.pfunc)  = 
+   pp_print_string fmt f.pf_name.id;
+   pp_print_char fmt '(';
+
+   pp_print_char fmt ')'
+
+
+
+   
+
+(*let rec ast_file (fmt : Format.formatter) (dl: Ast.pdecl list) = 
+   match dl with
+      |[] -> ()
+      |PDfunction(f)::t -> pp_open_box fmt 5;
+                           draw_pfunc fmt f;
+                           pp_close_box fmt ();
+                           pp_print_char fmt ';';
+                           ast_file fmt t
+      |PDstruct(s)::t -> ()
+
+*)
+
 let rec typ fmt = function
   | Tint -> fprintf fmt "int"
   | Tbool -> fprintf fmt "bool"
   | Tstring -> fprintf fmt "string"
   | Tstruct s -> fprintf fmt "%s" s.s_name
   | Tptr ty -> fprintf fmt "*%a" typ ty
+
+
   (* TODO autres types utilises par l'analyse semantique *)
 
 let rec expr fmt e = match e.expr_desc with
