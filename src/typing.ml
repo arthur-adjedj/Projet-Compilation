a273019759bbdf1f3d6ff875ecef90a853ab8340
arthur-adjedj <arthur.adjedj@gmail.com>
2021-12-02 09:25:00 +0100

    finished first part of the project

--- src/typing.ml
+++ src/typing.ml
@@ -124,7 +124,15 @@ and type_type = function
   | PTptr ty -> Tptr (type_type ty)
   | PTident {id = s } -> 
     let ns = pstruct_to_struct s in Tstruct ns
-      
+  
+let rec typstr = function
+  | Tint -> "int"
+  | Tbool ->  "bool"
+  | Tstring -> "string"
+  | Tstruct s -> s.s_name
+  | Tptr ty -> "*"^(typstr ty)
+  | Tmany l -> ""  (*A FAIRE*) 
+
 let rec eq_type ty1 ty2 = match ty1, ty2 with
   | Tint, Tint | Tbool, Tbool | Tstring, Tstring -> true
   | Tstruct s1, Tstruct s2 -> s1.s_name = s2.s_name
@@ -181,17 +189,31 @@ let rec is_l_value e = match e.expr_desc with
   |TEident(_) -> true
   |TEdot(el,_) -> is_l_value el
   |TEunop(Ustar,el) -> (
-    try
-      (let Tptr(_) = el.expr_typ in ());
-       el.expr_desc <> TEnil
-    with _ -> false)
+    match el.expr_typ with 
+      |Tptr _ -> el.expr_desc <> TEnil
+      |_ -> false
+    ) 
   |_ -> false
 
- let rec flatten = function
+let rec flatten = function
   |[] -> []
   |(Tmany a)::t -> (flatten a)@(flatten t)
   |h::t -> h::(flatten t)
   
+
+let errtyp loc exp real =
+  error loc ("type "^(typstr exp)^" expected, found "^(typstr real)^" instead") 
+
+
+let correct_assign nlvl nel =
+  let f1 = flatten (List.map (fun x -> x.expr_typ) nlvl)
+  and f2 = flatten (List.map (fun x -> x.expr_typ) nel) in
+  let rec aux = function
+    |[],[] -> true
+    |Tptr(_)::t1,Tptr(tvoid)::t2 -> aux (t1,t2)
+    |a::t1,b::t2 -> eq_type a b && aux (t1,t2)
+    |_ -> false
+in aux (f1,f2)
 
 let ret_type = ref tvoid
 
@@ -213,16 +235,22 @@ and expr_desc env loc = function
       let te1,te2 = (expr env e1),(expr env e2) in
       (match op with
         | Badd | Bsub | Bmul | Bdiv | Bmod -> 
-          if (fst te1).expr_typ <> Tint || (fst te2).expr_typ <> Tint then
-            error loc "type int expected";
+          if (fst te1).expr_typ <> Tint then
+            errtyp loc Tint (fst te1).expr_typ;
+          if (fst te2).expr_typ <> Tint then
+            errtyp loc Tint (fst te2).expr_typ;
           TEbinop(op,fst te1, fst te2), Tint,false
         | Beq | Bne | Blt | Ble | Bgt | Bge -> 
-          if (fst te1).expr_typ <> Tint || (fst te2).expr_typ <> Tint then
-            error loc "type int expected";
+          if (fst te1).expr_typ <> Tint  then
+            errtyp loc Tint (fst te1).expr_typ;
+          if (fst te2).expr_typ <> Tint  then
+            errtyp loc Tint (fst te2).expr_typ;
           TEbinop(op,fst te1, fst te2), Tbool,false
         | Band | Bor -> 
-          if (fst te1).expr_typ <> Tbool || (fst te2).expr_typ <> Tbool  then
-            error loc "type bool expected";
+          if (fst te1).expr_typ <> Tbool then
+            errtyp loc Tbool (fst te1).expr_typ;
+          if (fst te2).expr_typ <> Tbool then
+            errtyp loc Tbool (fst te2).expr_typ;
           TEbinop(op,fst te1, fst te2), Tbool,false)
   | PEunop (Uamp, e1) ->
       let son = fst (expr env e1) in
@@ -277,22 +305,20 @@ and expr_desc env loc = function
       let (ne,r1) = expr env e
       and (nb,r2) =  expr env b in
       if ne.expr_typ <> Tbool then
-        error loc "type bool expected"
-      else TEfor(ne,nb),tvoid,false
+        errtyp loc Tbool ne.expr_typ;
+      TEfor(ne,nb),tvoid,false
   | PEif (e1, e2, e3) ->
       let returns = ref false in
       let ne1,_ = expr env e1 
       and ne2,b2 = expr env e2 
       and ne3,b3 = expr env e3 in
       if ne1.expr_typ <> Tbool then
-        error loc "type bool expected"
-      else
+        errtyp loc Tbool ne1.expr_typ;
       returns := b2 && b3;
-      print_bool !returns;
       TEif(ne1,ne2,ne3),
       (if !returns then ne1.expr_typ else tvoid),
       !returns
-  | PEnil -> TEnil ,tvoid,false 
+  | PEnil -> TEnil ,Tptr(tvoid),false 
   | PEident {id=id} ->
      (try 
         let v = Env.find id !env in
@@ -304,7 +330,7 @@ and expr_desc env loc = function
       let s = match ne.expr_typ with
         |Tstruct a 
         |Tptr(Tstruct a)-> pstruct_to_struct a.s_name
-        |_ -> error loc "this type doesn't have any method"
+        |a -> error loc ("type "^(typstr a)^" doesn't have any method")
       in 
       if not (Hashtbl.mem s.s_fields id.id) then
         error loc ("structure "^s.s_name^" doesn't have method "^id.id)
@@ -315,67 +341,69 @@ and expr_desc env loc = function
       let nlvl = List.map (fun x -> fst (expr env x)) lvl
       and nel = List.map (fun x -> fst (expr env x)) el in
       if not (List.for_all is_l_value nlvl) then
-        error loc "ill-formed l-value"
-      else 
+        error loc "ill-formed l-value";
+      if not (correct_assign nlvl nel) then
+        error loc "wrong type assignation";
       TEassign (nlvl, nel), tvoid, false 
   | PEreturn el ->
       let sons = List.map (fun x -> fst (expr env x))  el in
       let ret = l_to_typ (List.map (fun x -> x.expr_typ) sons) in
       if ret <> !ret_type then
-        error loc "wrong return type";
+        errtyp loc !ret_type ret;
       TEreturn sons , tvoid, true
   | PEblock el ->
-    let curenv = !env in
-    let has_a_return = ref false in
-    let rec aux = function
-      |[] -> ()
-      |[(e,true)] -> has_a_return := true
-      |[(_,false)] -> 
-        if !has_a_return then error loc "return at the end of block expected";
-      |(e,b)::t -> 
-        if b then (
-          has_a_return := true;
-          if !ret_type <> tvoid && e.expr_typ <> !ret_type then
-            error loc "wrong type in block return";
-          ret_type := e.expr_typ
-        )
-        else aux t
-        in
-    let sons = List.map (fun x -> expr env x) el in
-    print_int (List.length sons);
-    aux sons;
-    env := curenv;
-    TEblock (List.map fst sons), tvoid, !has_a_return
+      let curenv = !env in
+      let has_a_return = ref false in
+      let rec aux = function
+        |[] -> ()
+        |[(e,true)] -> has_a_return := true
+        |[(_,false)] -> 
+          if !has_a_return then error loc "return at the end of block expected";
+        |(e,b)::t -> 
+          if b then (
+            has_a_return := true;
+            if !ret_type <> tvoid && e.expr_typ <> !ret_type then
+              errtyp loc !ret_type e.expr_typ;
+            ret_type := e.expr_typ
+          )
+          else aux t
+          in
+      let sons = List.map (fun x -> expr env x) el in
+      aux sons;
+      env := curenv;
+      TEblock (List.map fst sons), tvoid, !has_a_return
   | PEincdec(e, op) ->
-    let (ne,_) = expr env e in
-    if ne.expr_typ <> Tint then
-      error e.pexpr_loc "type int expected";
-    if not (is_l_value ne) then
-      error e.pexpr_loc "l-value expected";
-    TEincdec(ne,op),Tint,false
-  | PEvars(ids,ptyps,pexprs) ->
-    match ptyps with
-    |None -> 
-      if pexprs = [] then
-        error loc "empty declarations must be explicitly typed"
-      else let types = List.map (fun x -> (fst (expr env x)).expr_typ) pexprs in
-      (try 
-        let vars = List.map2 
-        (fun x y -> 
-          let next = Env.var x.id x.loc y !env in
-          env := fst next;
-          snd next) ids (flatten types) in
-        TEvars(vars),Tmany(types),false
-        with (Invalid_argument _) -> error loc "incorrect number of elements returned" )
-    |Some pt ->
-      let p = type_type pt in 
-      TEvars(
-        List.map 
-          (fun x -> 
-            let next = Env.var x.id x.loc p !env in
-            env := fst next;
-            snd next) ids),
-        tvoid,false
+      let (ne,_) = expr env e in
+      if ne.expr_typ <> Tint then
+        errtyp e.pexpr_loc Tint ne.expr_typ;
+      if not (is_l_value ne) then
+        error e.pexpr_loc "l-value expected";
+      TEincdec(ne,op),Tint,false
+  | PEvars(ids,ptyps,pexprs) -> (
+      match ptyps with
+        |None -> 
+          if pexprs = [] then
+            error loc "empty declarations must be explicitly typed"
+          else let types = List.map (fun x -> (fst (expr env x)).expr_typ) pexprs in
+            (try 
+              let vars = List.map2 
+                (fun x y -> 
+                  let next = Env.var x.id x.loc y !env in
+                  env := fst next;
+                  snd next) 
+                ids (flatten types) in
+              TEvars(vars),Tmany(types),false
+            with (Invalid_argument _) -> error loc "incorrect number of elements returned" )
+        |Some pt ->
+          let p = type_type pt in 
+          TEvars(
+            List.map 
+              (fun x -> 
+                let next = Env.var x.id x.loc p !env in
+                env := fst next;
+                snd next) ids),
+          tvoid,false
+    )
     
 
 
@@ -386,7 +414,7 @@ let found_main = ref false
 let phase1 = function
   | PDstruct ({ ps_name = { id = id; loc = loc }} as s) -> 
       if Pstructs.is_defed id then 
-        error loc ("structure"^"defined twice")
+        error loc ("structure"^id^"defined twice")
       else Pstructs.add_name s
   | PDfunction _ -> ()
 
@@ -406,15 +434,17 @@ let rec is_well_formed = function
   | PTptr ty -> is_well_formed ty
   | PTident ({id = s })-> Pstructs.is_defed s
 
-(*todo rajouter quels objets de f sont mal formés*)
-let phase2 = function
+
+  let phase2 = function
   | PDfunction ({ pf_name={id; loc}; pf_params=pl; pf_typ=tyl; } as f) ->
-    if id="main" then found_main := true;
+    if id="main" then (
+     if tyl <> [] then error loc "function main must have no return";
+     found_main := true);
      if List.for_all is_well_formed (List.map snd pl) && List.for_all is_well_formed tyl then 
       Funcs.add f
-    else error loc ("fonction :"^id^"is ill-formed")
+    else error loc ("fonction "^id^" is ill-formed")
   | PDstruct ({ ps_name = {id; loc}; ps_fields = fl } as s)->
-    if not ( List.for_all is_well_formed (List.map snd fl)) then 
+    if not (List.for_all is_well_formed (List.map snd fl)) then 
       error loc ("ill-formed types in structure :"^id);
     if not (Pstructs.are_fields_unique s) then
       error loc ("duplicate attributes in structure "^s.ps_name.id);
